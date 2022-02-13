cebf1da0177f94b254cca284a483d9b214d530b5
olegTervo <oleg.tervo-ridor@helsinki.fi>
2022-02-13 23:03:00 +0200

    heroku

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date().toString()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

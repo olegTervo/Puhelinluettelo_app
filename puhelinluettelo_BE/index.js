af7ef567f5da616fbc75eedce7db2923f2579224
olegTervo <oleg.tervo-ridor@helsinki.fi>
2022-02-13 22:40:20 +0200

    osa3 t9

--- puhelinluettelo_BE/index.js
+++ puhelinluettelo_BE/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', function getBody (req) {
   if(req.method && req.method === 'POST')
@@ -11,6 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     {
